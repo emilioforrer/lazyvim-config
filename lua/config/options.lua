68f3efb3c8d9f8bd743aab72bb7148c8577f92da
Emilio Forrer <dev.emilio.forrer@gmail.com>
2025-04-08 21:42:02 -0600

    ✨ Update Python LSP to ruff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,30 +2,28 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-
 -- ---------------------- SHARED NEOVIM SETTINGS ------------------------------
 
 -- Create a user command (:Z) to execute zoxide query
-vim.api.nvim_create_user_command('Z', function(opts)
-    local query = opts.args or ""
-    local handle = io.popen('zoxide query "' .. query .. '"')
-    if handle then
-      local result = handle:read("*a")
-      handle:close()
-      -- Trim whitespace from the result
-      local dir = result:match("^(.-)%s*$")
-      if dir and #dir > 0 then
-        -- Change Neovim's working directory
-        vim.cmd("cd " .. vim.fn.fnameescape(dir))
-        print("Directory changed to: " .. dir)
-      else
-        print("Error: No directory found for query: " .. query)
-      end
+vim.api.nvim_create_user_command("Z", function(opts)
+  local query = opts.args or ""
+  local handle = io.popen('zoxide query "' .. query .. '"')
+  if handle then
+    local result = handle:read("*a")
+    handle:close()
+    -- Trim whitespace from the result
+    local dir = result:match("^(.-)%s*$")
+    if dir and #dir > 0 then
+      -- Change Neovim's working directory
+      vim.cmd("cd " .. vim.fn.fnameescape(dir))
+      print("Directory changed to: " .. dir)
     else
-      print("Error: Failed to execute zoxide command")
+      print("Error: No directory found for query: " .. query)
     end
-  end, { nargs = "?" })
-
+  else
+    print("Error: Failed to execute zoxide command")
+  end
+end, { nargs = "?" })
 
 if vim.g.neovide then
   -- ---------------------- NEOVIDE SETTINGS ------------------------------
@@ -42,14 +40,10 @@ if vim.g.neovide then
   vim.g.neovide_cursor_vfx_mode = "railgun"
 end
 
-
-
 if vim.g.vscode then
   -- ---------------------- VSCODE NEOVIM SETTINGS------------------------------
-
 else
   -- ---------------------- NEOVIM ONLY SETTINGS------------------------------
-
 
   -- Configuration
 
@@ -61,24 +55,24 @@ else
   -- vim.o.secure = true
   -- vim.opt.clipboard = "unnamedplus"
 
+  vim.g.lazyvim_python_lsp = "ruff"
 
-  
   -- Enable list mode to show whitespace characters
   vim.opt.list = true
 
   -- Define the symbols for whitespace characters
   vim.opt.listchars = {
-      space = '·',       -- Character to show for space
-      tab = '→ ',        -- Character to show for tab
-      trail = '•',       -- Character to show for trailing spaces
-      eol = '↲',         -- Character to show for end of line
+    space = "·", -- Character to show for space
+    tab = "→ ", -- Character to show for tab
+    trail = "•", -- Character to show for trailing spaces
+    eol = "↲", -- Character to show for end of line
   }
 
   -- Optionally, you can customize the appearance of the whitespace characters using highlight groups
   -- For example, to change the color of the whitespace characters:
-  vim.cmd('highlight SpecialKey ctermfg=darkgray guifg=darkgray')
-  vim.cmd('highlight NonText ctermfg=darkgray guifg=darkgray')
-  vim.cmd('highlight EndOfBuffer ctermfg=darkgray guifg=darkgray')
+  vim.cmd("highlight SpecialKey ctermfg=darkgray guifg=darkgray")
+  vim.cmd("highlight NonText ctermfg=darkgray guifg=darkgray")
+  vim.cmd("highlight EndOfBuffer ctermfg=darkgray guifg=darkgray")
 
   -- Function to enable list mode
   local function enable_list_mode()
@@ -91,18 +85,17 @@ else
   end
 
   -- Autocommands to toggle list mode based on the mode
-  vim.api.nvim_create_autocmd('InsertEnter', {
-    pattern = '*',
-    callback = disable_list_mode
+  vim.api.nvim_create_autocmd("InsertEnter", {
+    pattern = "*",
+    callback = disable_list_mode,
   })
 
-  vim.api.nvim_create_autocmd('InsertLeave', {
-    pattern = '*',
-    callback = enable_list_mode
+  vim.api.nvim_create_autocmd("InsertLeave", {
+    pattern = "*",
+    callback = enable_list_mode,
   })
 
   -- Initially enable list mode when starting in normal mode
   enable_list_mode()
-
-  
 end
+
